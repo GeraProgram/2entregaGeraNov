89d29ac3f115ed317d15b5f4e612cbce508e582b
Gerardo Novau <novaugerardo@hotmail.com>
2023-08-19 17:47:27 -0300

    2entrega version 0.0.2

--- main.js
+++ main.js
@@ -6,45 +6,49 @@ class Clientes {
     this.localidad = localidad;
     this.producto = seleccion;
     }
-    bienvenida () { alert(`Bienvenido ${cliente1.nombre}. DNI: ${cliente1.dni} Localidad: ${localidad}`)}; 
+    bienvenida () { alert(`Bienvenido ${nombre}. DNI: ${dni} Localidad: ${localidad}`)}; 
 };
 
-const productos = ["prestamos", "seguros", "inversiones"];
+const productos = [{nombre: "prestamos", informacion: "Hasta 6 cuotas(50% Interes). Hasta 12 cuotas(100% Interes). Hasta 24 cuotas(300%)"},
+                   {nombre: "seguros", informacion:"Seguro de Automotor, Seguro de vida, Seguro de vivienda"},
+                   {nombre: "inversiones", informacion:" Inersiones de Vienes Raices. Inversiones en Activos"}
+ ];
 //console.log(productos);
 
-const producto = {
-    prestamos: "Hasta 6 cuotas(50% Interes). Hasta 12 cuotas(100% Interes). Hasta 24 cuotas(300%)",
-    seguros: "Seguro de Automotor, Seguro de vida, Seguro de vivienda",
-    inversiones: " Inersiones de Vienes Raices. Inversiones en Activos",
-};
+
+
+
+
 
 let nombre = prompt ("Ingrese nombre");
 let dni = parseInt(prompt("Ingrese numero de DNI"));
 let localidad = prompt ("Ingrese localidad");
 let seleccion = prompt("Que producto le interesa (prestamos, seguros, inversiones");
+let producto;
+
+    
+    
+  for (let index = 0; index < productos.length; index++) {
+        if (productos[index].nombre === seleccion) {
+            producto = productos[index];
+            
+      } //else { alert (`Lo que ud selecciono no esta dispible, tiene que elegit entre prestamos, seguros e inversiones.`);
+      //}
+
+};
+
+
+alert(`Producto: ${producto.nombre}. Informacion: ${producto.informacion}`);
 
 const cliente1 = new Clientes (  nombre, dni, localidad, seleccion
     );
 
-
-if (seleccion === "prestamos") {
-    alert(producto.prestamos);
-} else if (seleccion === "seguros") {
-    alert(producto.seguros);
-} else if (seleccion === "inversiones") {
-    alert(producto.inversiones);
-}else{ alert (`Lo que ud selecciono no esta dispible, tiene que elegit entre ${productos}`)
- 
-    seleccion = prompt("Que producto le interesa (prestamos, seguros, inversiones");
-
-    alert (producto[seleccion])
-}
-
- cliente1.bienvenida()
+cliente1.bienvenida()
 
 
 
 
 
-console.log(cliente1);
+
+
 
